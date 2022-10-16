6de665bd8bd46f4d4460429c0943a0324b8796fd
Wajjdi <wajdi.gaiech@etu.unice.fr>
2022-10-16 19:44:58 +0200

    css implementé

---
+++ tp_webcomponent/myComponents/equalizer/index.js
@@ -0,0 +1,182 @@
+import '../lib/webaudio-controls.js';
+
+
+const getBaseURL = () => {
+    return new URL('.',
+        import.meta.url);
+};
+
+
+class Equalizer extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({
+            mode: 'open'
+        });
+        this.createIds();
+        this.filters = [];
+    }
+
+    connectedCallback() {
+        this.shadowRoot.innerHTML = `
+        <style>
+        webaudio-knob{
+            margin-right:30px;} 
+            
+        </style>
+
+        <webaudio-knob 
+        id="freq_60" 
+        src="../assets/knobs/slider.png" 
+        value="1"  max="30" min="-30"  step="0.1"  sprites="127" 
+        valuetip="0" tooltip="frequence 60hz">
+        </webaudio-knob>
+         <webaudio-knob 
+         id="freq_170" 
+         src="../assets/knobs/slider.png" 
+         value="1"  max="30" min="-30"  step="0.1"  sprites="127" 
+         valuetip="0" tooltip="frequence 170hz">
+        </webaudio-knob>
+        <webaudio-knob 
+         id="freq_350" 
+         src="../assets/knobs/slider.png" 
+         value="1"  max="30" min="-30"  step="0.1"  sprites="127" 
+         valuetip="0" tooltip="frequence 350hz">
+        </webaudio-knob>
+        <webaudio-knob 
+         id="freq_1000" 
+         src="../assets/knobs/slider.png" 
+         value="1"  max="30" min="-30"  step="0.1"  sprites="127" 
+         valuetip="0" tooltip="frequence 1000hz">
+        </webaudio-knob>
+        <webaudio-knob 
+         id="freq_3500" 
+         src="../assets/knobs/slider.png" 
+         value="1"  max="30" min="-30"  step="0.1"  sprites="127" 
+         valuetip="0" tooltip="frequence 3500hz">
+        </webaudio-knob>
+        <webaudio-knob 
+        id="freq_10000" 
+        src="../assets/knobs/slider.png" 
+        value="1"  max="30" min="-30"  step="0.1"  sprites="127" 
+        valuetip="0" tooltip="frequence 10000hz">
+       </webaudio-knob>
+      </label>
+        </div>
+        `;
+        this.getElements();
+
+        this.init();
+        this.defineListeners();
+        this.fixRelativeURLs()
+    }
+
+    fixRelativeURLs() {
+        const baseURL = getBaseURL();
+        console.log('baseURL 2', baseURL);
+
+        const knobs = this.shadowRoot.querySelectorAll('webaudio-knob');
+
+        for (const knob of knobs) {
+            console.log("fixing " + knob.getAttribute('src'));
+
+            const src = knob.src;
+            knob.src = baseURL + src;
+
+            console.log("new value : " + knob.src);
+        }
+    }
+    init() {
+        const interval = setInterval(() => {
+            if (this.audioContext) {
+
+                [60, 170, 350, 1000, 3500, 10000].forEach((freq, i) => {
+                    const eq = this.audioContext.createBiquadFilter();
+                    eq.frequency.value = freq;
+                    eq.type = "peaking";
+                    eq.gain.value = 0;
+                    this.filters.push(eq);
+                });
+
+                this.filters.forEach((filter) => {
+                    this.addAudioNode(filter);
+                });
+
+                clearInterval(interval);
+            }
+        }, 100);
+    }
+
+    changeGain(nbFilter, sliderVal) {
+        this.filters[nbFilter].gain.value = parseFloat(sliderVal);
+
+    }
+
+    createIds() {
+        this.ids = {
+            FREQ_60: 'freq_60',
+            FREQ_170: 'freq_170',
+            FREQ_350: 'freq_350',
+            FREQ_1000: 'freq_1000',
+            FREQ_3500: 'freq_3500',
+            FREQ_10000: 'freq_10000',
+        };
+    }
+
+    getElements() {
+        this.freq_60 = this.shadowRoot.getElementById(this.ids.FREQ_60);
+        this.freq_170 = this.shadowRoot.getElementById(this.ids.FREQ_170);
+        this.freq_350 = this.shadowRoot.getElementById(this.ids.FREQ_350);
+        this.freq_1000 = this.shadowRoot.getElementById(this.ids.FREQ_1000);
+        this.freq_3500 = this.shadowRoot.getElementById(this.ids.FREQ_3500);
+        this.freq_10000 = this.shadowRoot.getElementById(this.ids.FREQ_10000);
+    }
+    defineListeners() {
+        this.freq_60.addEventListener('input', ({
+            target: {
+                value
+            }
+        }) => {
+            this.changeGain(1, value);
+            console.log("je change");
+        });
+        this.freq_170.addEventListener('input', ({
+            target: {
+                value
+            }
+        }) => {
+            this.changeGain(1, value);
+        });
+        this.freq_350.addEventListener('input', ({
+            target: {
+                value
+            }
+        }) => {
+            this.changeGain(2, value);
+        });
+        this.freq_1000.addEventListener('input', ({
+            target: {
+                value
+            }
+        }) => {
+            this.changeGain(3, value);
+        });
+        this.freq_3500.addEventListener('input', ({
+            target: {
+                value
+            }
+        }) => {
+            this.changeGain(4, value);
+        });
+        this.freq_10000.addEventListener('input', ({
+            target: {
+                value
+            }
+        }) => {
+            this.changeGain(5, value);
+        });
+    }
+
+}
+
+customElements.define('my-equalizer', Equalizer);
--- tp_webcomponent/myComponents/index.js
+++ tp_webcomponent/myComponents/index.js
@@ -1,6 +1,7 @@
 import './lib/webaudio-controls.js';
+import './equalizer/index.js';
 
-
+const AudioContext = window.AudioContext || window.webkitAudioContext;
 const getBaseURL = () => {
   return new URL('.',
     import.meta.url);
@@ -12,28 +13,26 @@ class myComponent extends HTMLElement {
     this.attachShadow({
       mode: 'open'
     });
-    this.playList = [
-      {
-        url: getBaseURL()+"assets/Gangstas_Paradise.mp3",
+    this.playList = [{
+        url: getBaseURL() + "assets/Gangstas_Paradise.mp3",
         author: "Kavinsky",
         title: "Nightcall",
         index: 0,
       },
       {
-        url: getBaseURL()+"assets/Daft_Punk_Technologic.mp3",
+        url: getBaseURL() + "assets/Daft_Punk_Technologic.mp3",
         author: "Daft Punk",
         title: "Technologic",
         index: 1,
       },
       {
-        url: getBaseURL()+"assets/113_Tonton_Du_Bled.mp3",
+        url: getBaseURL() + "assets/113_Tonton_Du_Bled.mp3",
         author: "113",
         title: "Tonton du bled",
         index: 2,
       },
     ];
     this.currentSoundObject = this.playList[0];
-    this.createIds();
     this.filters = [];
   }
 
@@ -44,15 +43,82 @@ class myComponent extends HTMLElement {
         <style>
             h1 {
                 color:red;
+                font-family: Georgia, serif;
+            }
+            label { 
+              font-family:serif;
             }
             #myCanvas {
               border:1px solid;
             }
+            .card-body {
+              text-align:center;  /*1*/
+            padding: 15px 20px; /*2*/
+            box-sizing: border-box; /*3*/
+            border-radius: 20px;
+            background: #f2f2f2;
+          }
+            button {
+              appearance: none;
+              background-color: #FAFBFC;
+              border: 1px solid rgba(27, 31, 35, 0.15);
+              border-radius: 6px;
+              box-shadow: rgba(27, 31, 35, 0.04) 0 1px 0, rgba(255, 255, 255, 0.25) 0 1px 0 inset;
+              box-sizing: border-box;
+              color: #24292E;
+              cursor: pointer;
+              display: inline-block;
+              font-family: -apple-system, system-ui, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+              font-size: 14px;
+              font-weight: 500;
+              line-height: 20px;
+              list-style: none;
+              padding: 6px 16px;
+              position: relative;
+              transition: background-color 0.2s cubic-bezier(0.3, 0, 0.5, 1);
+              user-select: none;
+              -webkit-user-select: none;
+              touch-action: manipulation;
+              vertical-align: middle;
+              white-space: nowrap;
+              word-wrap: break-word;
+            }
+            button:hover {
+              background-color: #F3F4F6;
+              text-decoration: none;
+              transition-duration: 0.1s;
+            }
+
+            button:disabled {
+              background-color: #FAFBFC;
+              border-color: rgba(27, 31, 35, 0.15);
+              color: #959DA5;
+              cursor: default;
+            }
+
+            button:active {
+              background-color: #EDEFF2;
+              box-shadow: rgba(225, 228, 232, 0.2) 0 1px 0 inset;
+              transition: none 0s;
+            }
+
+            button:focus {
+              outline: 1px transparent;
+            }
+
+            .button:before {
+              display: none;
+            }
+
+            button:-webkit-details-marker {
+              display: none;
+            }
         </style>
-        <h1>lecteur audio amélioré</h1>
-        <canvas id="myCanvas" width=250 height=110></canvas>
+        <div class="card-body">
+        <h1>Lecteur audio Wajdi Gaiech</h1>
+        <canvas id="myCanvas" width=500 height=130></canvas>
         <br>
-        <audio id="player" src="${this.currentSoundObject.url}" controls></audio>
+        <audio id="player" src="${this.currentSoundObject.url}" style="visibility: hidden;" controls ></audio>
         <br>
         <button id="play">Play</button>
         <button id="pause">Pause</button>
@@ -61,54 +127,50 @@ class myComponent extends HTMLElement {
         <button id="avance">10sec</button>
         <button id="precedent">Precedent</button>
         <button id="suivant">Suivant</button>
-        <button id="loop">loop</button>
+        <button id="loop" >loop</button>
         <br>
-        <label>Volume 
-          <input id="volumeSlider" 
-          type="range" min=0 max=1 step=0.1 value="1">
+        <label style="margin-right: 30%;">Volume 
+        </label>
+        <label>frequence 
         </label>
         <br>
-        <label>frequence 
-        <input id="freq_60" 
-        type="range" min=-30 max=30 step=0.1 value="1">
-        <input id="freq_170" 
-        type="range" min=-30 max=30 step=0.1 value="1">
-        <input id="freq_350" 
-        type="range" min=-30 max=30 step=0.1 value="1">
-        <input id="freq_1000" 
-        type="range" min=-30 max=30 step=0.1 value="1">
-        <input id="freq_3500" 
-        type="range" min=-30 max=30 step=0.1 value="1">
-        <input id="freq_10000" 
-        type="range" min=-30 max=30 step=0.1 value="1">
-      </label>
-      <br>
+        <webaudio-knob 
+        id="volumeKnob" 
+        src="./assets/knobs/vernier.png" 
+        value="1" max="1" step="0.1" diameter="128" sprites="50" 
+        valuetip="0" tooltip="Volume" style= "margin-right: 10%;margin-left: 15%;">
+      </webaudio-knob>
+      <my-equalizer id="equalizer"></my-equalizer>
+       <br>
+       
       <label>Balance </label>
-      <input id="balance" 
-      type="range" min=-1 max=1 step=0.1 value="1">
+      <br>
+      <webaudio-knob 
+        id="balance" 
+        src="./assets/knobs/slider_balance.png" 
+        value="0" max="1" min="-1" step="0.1"  sprites="30" 
+        valuetip="0" tooltip="Balance">
+      </webaudio-knob>
     </label>
         <br>
-        <webaudio-knob 
-          id="volumeKnob" 
-          src="./assets/knobs/vernier.png" 
-          value="1" max="1" step="0.1" diameter="128" sprites="50" 
-          valuetip="0" tooltip="Volume">
-        </webaudio-knob>
+       
     `;
 
-
+   
     this.url = getBaseURL();
     console.log(this.url);
     this.getElements();
+    
     this.buildGraph();
+    this.initDependencies();
     this.fixRelativeURLs();
     this.player.onplay = () => {
       // pour démarrer webaudio lors d'un click...
       console.log("play");
       this.audioContext.resume();
-  
+
     }
-   
+
     // pour dessiner/animer
     this.init();
     this.defineListeners();
@@ -121,30 +183,31 @@ class myComponent extends HTMLElement {
 
   }
 
+
   // ---------------------------------- Animer une ligne de frequence ---------------------------------- //
+
   animation() {
     if (!this.analyser) {
       setTimeout(() => {
-          requestAnimationFrame(() => this.animation());
-      }, 100);
+        requestAnimationFrame(() => this.animation());
+      }, 500);
       return;
-  }
-  
-  this.canvasContext.clearRect(0, 0, this.width, this.height);
-  this.analyser.getByteFrequencyData(this.dataArray);
+    }
+    this.canvasContext.clearRect(0, 0, this.width, this.height);
+    this.analyser.getByteFrequencyData(this.dataArray);
 
-  const barWidth = this.width / this.bufferLength;
-  var barHeight;
-  var x = 0;
-  const heightScale = this.height / 128;
+    const barWidth = this.width / this.bufferLength;
+    var barHeight;
+    var x = 0;
+    const heightScale = this.height / 128;
 
-  for(var i = 0; i < this.bufferLength; i++) {
+    for (var i = 0; i < this.bufferLength; i++) {
       barHeight = this.dataArray[i];
-      this.canvasContext.fillStyle = 'rgb(176, 38, 255)';
+      this.canvasContext.fillStyle = "#33BBFF";
       barHeight *= heightScale;
       this.canvasContext.fillRect(x, this.height - barHeight / 2, barWidth, barHeight / 2);
       x += barWidth + 1;
-  }
+    }
     // 3 - on rappelle la fonction dans 1/60ème de seconde
     requestAnimationFrame(() => {
       this.animation();
@@ -187,79 +250,57 @@ class myComponent extends HTMLElement {
       console.log("new value : " + knob.src);
     }
   }
+
+
+
+
   // ---------------------------------- initialisation ---------------------------------- // 
+  initDependencies() {
+    //equalizer
+    this.equalizer.audioContext = this.audioContext;
+    this.equalizer.addAudioNode = (audioNode) => this.addAudioNode(audioNode, "equalizer");
+  }
+
   init() {
     const interval = setInterval(() => {
       if (this.audioContext) {
-  
-        
+
+
         // ------------------ Balance ------------------ //
         this.pannerNode = this.audioContext.createStereoPanner();
         this.addAudioNode(this.pannerNode);
+        
+        // ----------------------- canvas ------------------------------- //
+        // mettre apres tout les filtre dans l'initialisation pour voir le changement des frequnce sur le canvas
+        this.analyser = this.audioContext.createAnalyser();
 
+        this.analyser.fftSize = 256;
+        this.bufferLength = this.analyser.frequencyBinCount;
+        this.dataArray = new Uint8Array(this.bufferLength);
 
-        // -------------------  Filter ------------------ //
-        [60, 170, 350, 1000, 3500, 10000].forEach((freq, i) => {
-          const eq = this.audioContext.createBiquadFilter();
-          eq.frequency.value = freq;
-          eq.type = "peaking";
-          eq.gain.value = 0;
-          this.filters.push(eq);
-        });
-
-        this.filters.forEach((filter) => {
-          this.addAudioNode(filter);
-        });
-
-      // ----------------------- canvas ------------------------------- //
-      // mettre apres tout les filtre dans l'initialisation pour voir le changement des frequnce sur le canvas
-      this.analyser = this.audioContext.createAnalyser();
-
-      this.analyser.fftSize = 256;
-      this.bufferLength = this.analyser.frequencyBinCount;
-      this.dataArray = new Uint8Array(this.bufferLength);
-
-      this.addAudioNode(this.analyser);
+        this.addAudioNode(this.analyser);
         clearInterval(interval);
       }
-    }, 100);
+    }, 200);
     this.width = this.canvas.width;
-        this.height = this.canvas.height;
-        this.canvasContext = this.canvas.getContext('2d');
+    this.height = this.canvas.height;
+    this.canvasContext = this.canvas.getContext('2d');
 
   }
 
-  // --------------------------------- Frequence ---------------------------------- //
-  changeGain(nbFilter, sliderVal) {
-    this.filters[nbFilter].gain.value = parseFloat(sliderVal);
-  }
 
+
+  // --------------------------------- changer de chanson ---------------------------------- //
+  
   updateCurrentPlayerSong(song) {
     this.currentSoundObject = song;
     this.player.src = this.currentSoundObject.url;
     this.player.play();
   }
 
-  createIds() {
-    this.ids = {
-      FREQ_60: 'freq_60',
-      FREQ_170: 'freq_170',
-      FREQ_350: 'freq_350',
-      FREQ_1000: 'freq_1000',
-      FREQ_3500: 'freq_3500',
-      FREQ_10000: 'freq_10000',
-      BALANCE: 'balance',
-    };
-  }
-
   getElements() {
-    this.freq_60 = this.shadowRoot.getElementById(this.ids.FREQ_60);
-    this.freq_170 = this.shadowRoot.getElementById(this.ids.FREQ_170);
-    this.freq_350 = this.shadowRoot.getElementById(this.ids.FREQ_350);
-    this.freq_1000 = this.shadowRoot.getElementById(this.ids.FREQ_1000);
-    this.freq_3500 = this.shadowRoot.getElementById(this.ids.FREQ_3500);
-    this.freq_10000 = this.shadowRoot.getElementById(this.ids.FREQ_10000);
-    this.balance = this.shadowRoot.getElementById(this.ids.BALANCE);
+   
+    this.balance = this.shadowRoot.getElementById("balance");
     this.canvas = this.shadowRoot.querySelector('#myCanvas');
     this.player = this.shadowRoot.querySelector('#player');
     this.volumeknob = this.shadowRoot.querySelector('#volumeKnob');
@@ -267,6 +308,8 @@ class myComponent extends HTMLElement {
     this.precedent = this.shadowRoot.getElementById("precedent");
     this.suivant = this.shadowRoot.getElementById("suivant");
     this.boucle = this.shadowRoot.getElementById("loop");
+    this.equalizer = this.shadowRoot.getElementById("equalizer");
+    this.freq_visualiser = this.shadowRoot.getElementById("freq_visualiser");
   }
   // ---------------------------------- Listeners ---------------------------------- //
   defineListeners() {
@@ -275,12 +318,15 @@ class myComponent extends HTMLElement {
     });
     this.boucle.addEventListener('click', () => {
       if (this.player.loop === false) {
+        console.log("loop");
         this.player.loop = true;
-
-      }else{
+        this.boucle.style.color = "green";
+      } else {
+        console.log("no loop");
         this.player.loop = false;
+        this.boucle.style.color = "black";
       }
-    
+
     });
     this.shadowRoot.querySelector('#pause').addEventListener('click', () => {
       this.player.pause();
@@ -296,56 +342,9 @@ class myComponent extends HTMLElement {
     this.shadowRoot.querySelector('#avance').addEventListener('click', () => {
       this.player.currentTime = this.player.currentTime + 10;
     });
-    this.shadowRoot.querySelector('#volumeSlider').addEventListener('input', (evt) => {
-      this.player.volume = evt.target.value;
-      this.volumeknob.value = evt.target.value;
-    });
     this.shadowRoot.querySelector('#volumeKnob').addEventListener('input', (evt) => {
       this.player.volume = evt.target.value;
-      this.volumeslider.value = evt.target.value;
-    });
-    this.freq_60.addEventListener('input', ({
-      target: {
-        value
-      }
-    }) => {
-      this.changeGain(1, value);
-      console.log("je change");
-    });
-    this.freq_170.addEventListener('input', ({
-      target: {
-        value
-      }
-    }) => {
-      this.changeGain(1, value);
-    });
-    this.freq_350.addEventListener('input', ({
-      target: {
-        value
-      }
-    }) => {
-      this.changeGain(2, value);
-    });
-    this.freq_1000.addEventListener('input', ({
-      target: {
-        value
-      }
-    }) => {
-      this.changeGain(3, value);
-    });
-    this.freq_3500.addEventListener('input', ({
-      target: {
-        value
-      }
-    }) => {
-      this.changeGain(4, value);
-    });
-    this.freq_10000.addEventListener('input', ({
-      target: {
-        value
-      }
-    }) => {
-      this.changeGain(5, value);
+
     });
     this.balance.addEventListener('input', ({
       target: {
@@ -367,7 +366,7 @@ class myComponent extends HTMLElement {
       }
       this.updateCurrentPlayerSong(song);
     });
-    
+
     this.suivant.addEventListener('click', () => {
       const length = this.playList.length;
       let song;
@@ -380,7 +379,9 @@ class myComponent extends HTMLElement {
       }
       this.updateCurrentPlayerSong(song);
     });
-    this.player.onplay = (e) => { this.audioContext.resume(); };
+    this.player.onplay = (e) => {
+      this.audioContext.resume();
+    };
 
   }
 
